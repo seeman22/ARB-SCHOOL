6e9b93c6a693df58b1dd36bdace5d38e0222173f
seeman22 <seeman79202@gmail.com>
2026-01-12 11:52:39 +0530

    Initial project commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Codissia Innovation 
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <?php include('./includes/head.php'); ?>
+
+</head>
+<body>
+        <?php include('./includes/header.php'); ?>
+
+</body>
+</html>
